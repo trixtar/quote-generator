3c22cfba826f697af008e69b83437fe8342bab58
trixtar <magisterludi1789@gmail.com>
2017-07-27 02:07:26 -0300

    working version

--- js/js3.js
+++ js/js3.js
@@ -1,8 +1,19 @@
+// VARIABLES
+
+var colors = ['#306754', '#A52A2A', '#D2691E', '#DC143C', '#00008B', '#006400', '#8B0000', '#483D8B', '#2F4F4F', '#9400D3', '#008000', '#FF4500'];
+
+var quote = {};
+
+
 // FUNCTIONS
 
-// New Quote
+// Get Random Integer
 
-var quote;
+function getRandomInt(min, max) {
+  return Math.floor(Math.random() * (max - min)) + min;
+}
+
+// New Quote
 
 function newQuote(){	
 
@@ -17,13 +28,49 @@ function newQuote(){
 			lang: 'en',
 		},
 		success: function(data) {
-			quote = data;
-			console.log(quote);
+			quote.text = data.quoteText;
+			if (data.quoteAuthor) {
+				quote.author = ' - ' + data.quoteAuthor;
+			} else {
+				quote.author = ' - Unknown';
+			}
+			
+			$('#quote').text(quote.text);
+			$('#author').text(quote.author);
+
+			var col = getRandomInt(0,colors.length);
+			$('main').css('backgroundColor', colors[col]);
+			$('.quote-text').css('color', colors[col]);
+			$('.author').css('color', colors[col]);
 		}
-
 	});
+}; //newQuote
 
-}; 
+
+// TruncateTweet
+
+function truncateTweet(arr, num) {
+    var trunc = arr.splice(0, arr.length-num).join('').concat('...');
+    return trunc;
+}
+
+var textArray = [];
+
+function tweetQuote() {
+	textArray = quote.text.split('').slice(0, -1);
+	var suma = textArray.length + quote.author.length;
+	if (suma > 140) {
+		var surplus = suma - 140 + 3;
+		quote.text = truncateTweet(textArray, surplus);
+	} else {quote.text = textArray.join('')}
+	quote.tweet = quote.text + quote.author;
+	quote.tweetCode = (quote.tweet).split(' ').join('%20');
+	$('#twitter').attr('href','https://twitter.com/intent/tweet/?text=' + quote.tweetCode);
+	console.log(quote.tweet);
+	console.log(quote.tweetCode);
+}
+
+
 
 
 
@@ -44,9 +91,11 @@ $('#new-quote').on('click', function(){
 $('#twitter').on('click', function(){
 
 	if (quote) {
-
+		tweetQuote();
+		var url = 'https://twitter.com/intent/tweet/?text=' + quote.tweetCode;
+		window.location.href = url;
 	} else {
-		alert('Get a quote first!')
+		alert('Generate a new quote first!')
 	}
 
 
